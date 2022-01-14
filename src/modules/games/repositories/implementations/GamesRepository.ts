486cdc7e69edc26445775ec5733f1fa36d07a488
RodrigoSester <rodrigows09@gmail.com>
2022-01-14 15:29:06 -0300

    Implement repositories

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,24 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
+      .select("title")
+      .from(Game, "games")
+      .where(`%${param}% = title`)
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(id) FROM games`); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .select("*")
+      .from(User, "users, games, users_games_games")
+      .where(`users.id = users_games_games AND games.${id} = users_games_games`)
+      .getMany()
       // Complete usando query builder
   }
 }
